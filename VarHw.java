05ffb5f0b20842093e039cc2b6a16840fc7e9e45
kyang19 <42977150+kyang19@users.noreply.github.com>
2018-09-13 13:39:51 -0400

    Add files via upload

---
+++ VarHw.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @author kimyang
+ * Period 6
+ */
+
+public class VarHw 
+{
+
+	public static void main(String[] args) 
+	{
+		String name = "Kimberly";
+		System.out.print(name);
+		
+		int length = 4;
+		int width  = 3;
+		int area= length*width;
+		
+		System.out.print("\n" + "\n" + area);
+		
+		int radius = 4;
+		double pi = 3.14;
+		int formula = 2;
+		double circumferenceOfCircle= radius*pi*formula;
+		
+		System.out.print("\n"+"\n"+ "Circumference: " + circumferenceOfCircle);
+		
+		double areaOfCircle=radius*radius*pi;
+		System.out.print("\n"+"\n"+ "Area: "+ areaOfCircle);
+
+	}
+
+}
